4e4996e1a8726ffd4d89c0a1194fe8110d76e451
Loeka1234 <loekalievens@hotmail.com>
2020-08-17 22:13:09 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,33 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
 import nodemailer from "nodemailer";
 import cors from "cors";
 
 dotenv.config();
 
+const API_KEY = process.env.API_KEY;
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+const protectedRoute = (req: Request, res: Response, next: NextFunction) => {
+    const { apiKey } = req.body;
+    if (apiKey !== API_KEY?.toString()) res.sendStatus(401);
+    else next();
+};
+
+const transport = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.MAIL,
+        pass: process.env.PASSWORD,
+    },
+});
+
 app.get("/", (req, res) => res.send("Mail server working."));
-app.post("/contactmail", (req, res) => {
+app.post("/contactmail", protectedRoute, (req, res) => {
     const { email, message, to } = req.body;
     const emailValidation = /\S+@\S+\.\S+/;
     if (!emailValidation.test(email))
@@ -23,25 +39,25 @@ app.post("/contactmail", (req, res) => {
     if (!message || !message.trim())
         return res.status(400).json({ error: "Please provide a message! " });
 
-    const transport = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: process.env.MAIL,
-            pass: process.env.PASSWORD,
-        },
-    });
     const emailToSend = {
         from: process.env.MAIL,
         to: email,
-        subject: `${email} contacted you! | Mailserver`,
+        subject: `${email} contacted you! | Mailserver Loeka`,
         html: `<p>Message from ${email}</p><p>${message}</p>`,
     };
-    transport.sendMail(emailToSend).then(response => {
-        console.log(response);
-        res.sendStatus(200);
-    }).catch(console.log);
+    transport
+        .sendMail(emailToSend)
+        .then(response => {
+            console.log(`Email from ${email} to ${response.envelope.to}`);
+            res.sendStatus(200);
+        })
+        .catch(console.log);
 });
 
 const PORT = 4000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Mail server listening on port ${PORT}`));
+app.listen(PORT, () =>
+    console.log(
+        `Mail server listening on port ${PORT} using API key: ${API_KEY}`
+    )
+);
